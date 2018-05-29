8a7a8597bf58376fe2f955c3f7af15d41b5e2aff
erx <ervin.hylp@gmail.com>
2018-05-29 21:28:14 +0300

    shorter

--- timeout2.py
+++ timeout2.py
@@ -3,18 +3,13 @@ from time import ctime, sleep, time
 
 k = 0
 
-
 def some_task():
     global k
     while True:
         k = k + 1
 
 
-t = Thread(target=some_task)  # run the some_task function in another
-# thread
-t.daemon = True  # Python will exit when the main thread
-# exits, even if this thread is still
-# running
+t = Thread(target=some_task, daemon=True)  # run the some_task function in another
 print('Start at', ctime(time()))
 t.start()
 sleep(9)
